859a962b8c37ec3d5cea4cf8520aebaa5da60eb9
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-01-13 15:46:35 +0200

    Refactored movieAPI object.

--- src/js/1initialHomePage.js
+++ src/js/1initialHomePage.js
@@ -2,14 +2,57 @@
 // Консолька для отслеживания, что JS файлы подключаются в верном порядке. Просто параноя уже по Gulp-у
 console.log('1');
 
-// Объект с данными и методами для работы с The MovieDB. Пока такой, будет пополняться по мере рефакторинга
+// Объект с данными и методами для работы с The MovieDB.
 const movieApi = {
   apiKey: '0757258023265e845275de2a564555e9',
   baseUrl: 'https://api.themoviedb.org/3/',
-  baseImageUrl: 'https://image.tmdb.org/t/p/',
+  searchQuery: '',
+  totalResults: 0,
+  perPage: 20,
+  totalPages: 0,
+  pageNumber: 1,
+  isLastPage: false,
+  images: {
+    baseImageUrl: 'https://image.tmdb.org/t/p/',
+    backdrop_size: {
+      mobile: 'w300',
+      tablet: 'w780',
+      desktop: 'w1280',
+      original: 'original',
+    },
+    poster_size: {
+      mobile: 'w342',
+      tablet: 'w500',
+      desktop: 'w780',
+      original: 'original',
+    },
+  },
+
+  incrementPage() {
+    this.pageNumber += 1;
+  },
+  resetPage() {
+    this.pageNumber = 1;
+  },
+  get query() {
+    return this.searchQuery;
+  },
+  set query(newSearchQuery) {
+    this.searchQuery = newSearchQuery;
+  },
+  fetchPopularMoviesList() {
+    return fetch(
+      `${this.baseUrl}movie/popular?api_key=${this.apiKey}&language=en-US&page=${this.pageNumber}`,
+    )
+      .then(response => response.json())
+      .then(({ results }) => results);
+  },
+  fetchGenres() {
+    return fetch(`${this.baseUrl}genre/movie/list?api_key=${this.apiKey}`)
+      .then(response => response.json())
+      .then(data => data.genres);
+  },
 };
-// Консолька для мониторинга состояния данных в объете для работы с API
-console.log(movieApi);
 
 // Доступ к списку на домашней странице. В этот список будут рендерится популярные фильмы
 // при загрузке страницы, и фильмы - результат поиска
@@ -17,13 +60,35 @@ const homePageRef = document.querySelector('[data-home-gallery]');
 
 // Глобальные переменные, которые требуются по инструкции
 let renderFilms = '';
-let genres = '';
-let pageNumber = 1;
+const genres = movieApi.fetchGenres(); // содержит промис с массивом объектов жанров
+let pageNumber = 1; // можно заменить свойством в АПИШКЕ
 
 // Функции
-// Функция, которая создает одну карточку для галереи и которая вешает на карточку слушатель события для показа страницы деталей фильма
-function createCardFunc(imgPath, filmTitle, movieId, movieRating) {
-  // Создание карточки фильма. Карточка - это лишка, в которой есть изображение и название фильма
+// Функция, которая рендерит (вставляет в DOM) всю страницу галереи. Принимает фрагмент и ссылку, куда надо вставить фрагмент.
+function renderGallery(fragment, place) {
+  place.appendChild(fragment);
+}
+
+// Функция, которая создает фрагмент со всеми карточками галереи. Принимает массив объектов фильмов.
+function createGallery(movies) {
+  const galleryFragment = document.createDocumentFragment();
+
+  movies.forEach(movie => {
+    const galleryItem = createCardFunc(movie);
+    galleryFragment.appendChild(galleryItem);
+  });
+
+  return galleryFragment;
+}
+
+// Функция, которая создаёт одну карточку для фильма. Принимает один объект фильма.
+function createCardFunc(movie) {
+  const imgPath = movieApi.images.baseImageUrl + 'w500' + movie.backdrop_path;
+  const filmYear = movie.release_date.slice(0, 4);
+  const filmTitle = `${movie.title} (${filmYear})`;
+  const movieId = movie.id;
+  const movieRaiting = movie.vote_average;
+
   const galleryItemCard = document.createElement('li');
   galleryItemCard.classList.add('gallery-item-card');
   galleryItemCard.setAttribute('data-id', movieId);
@@ -34,68 +99,29 @@ function createCardFunc(imgPath, filmTitle, movieId, movieRating) {
   const galleryItemTitle = document.createElement('p');
   galleryItemTitle.classList.add('gallery-card-title');
   galleryItemTitle.textContent = filmTitle;
+
   const galleryItemRating = document.createElement('p');
   galleryItemRating.classList.add('gallery-card-raiting');
-  galleryItemRating.textContent = movieRating;
+  galleryItemRating.textContent = movieRaiting;
 
   galleryItemCard.appendChild(galleryItemRating);
   galleryItemCard.appendChild(galleryItemImage);
   galleryItemCard.appendChild(galleryItemTitle);
 
-  // Добавление слушателя события, чтобы открыть страницу с деталями.
-  // Пока так по инструкции. Но потом стоит переделать на делегирование.
-  // Иначе у нас миллион слушателей будет.
-  galleryItemCard.addEventListener('click', () => {
-    // Консолька для проверки, что слушатель события еще на месте.
-    console.log('Hello, I am click event!');
-    activeDetailsPage(movieId, false);
-  });
   return galleryItemCard;
 }
 
-// Функция, которая фетчит список популярных фильмов с API. Далее она создает карточку для каждого объекта результата и встраивает в список на
-//  домашней странице через фрагмент.
-function fetchPopularMoviesList() {
-  return fetch(
-    `${movieApi.baseUrl}movie/popular?api_key=${movieApi.apiKey}&language=en-US&page=${pageNumber}`,
-  )
-    .then(res => res.json())
-    .then(({ results }) => {
-      console.log(results);
+// // Добавление слушателя события, чтобы открыть страницу с деталями.
+// // Пока так по инструкции. Но потом стоит переделать на делегирование.
+// // Иначе у нас миллион слушателей будет.
+// galleryItemCard.addEventListener('click', () => {
+//   // Консолька для проверки, что слушатель события еще на месте.
+//   console.log('Hello, I am click event!');
+//   activeDetailsPage(movieId, false);
+// });
 
-      return results;
-    })
-    .then(movies => {
-      const popularMoviesFragment = document.createDocumentFragment();
-      movies.forEach(movie => {
-        const imgPath = movieApi.baseImageUrl + 'w500' + movie.backdrop_path;
-        const filmYear = movie.release_date.slice(0, 4);
-        const filmTitle = `${movie.title} (${filmYear})`;
-        const movieId = movie.id;
-        const movieRaiting = movie.vote_average;
-        const popularMoviesItem = createCardFunc(
-          imgPath,
-          filmTitle,
-          movieId,
-          movieRaiting,
-        );
-
-        popularMoviesFragment.appendChild(popularMoviesItem);
-      });
-
-      return popularMoviesFragment;
-    })
-    .then(fragment => homePageRef.appendChild(fragment));
-}
-
-function fetchGenres() {
-  return fetch(
-    `${movieApi.baseUrl}genre/movie/list?api_key=${movieApi.apiKey}&language=en-US`,
-  )
-    .then(res => res.json())
-    .then(genresObj => genresObj.genres);
-}
-
-fetchPopularMoviesList();
-fetchGenres();
-// console.log("pageNumber", pageNumber);
+// Вызов функций, которые фетчат популярные фильмы, создают фразмент с карточками галереи для каждого фильма и рендерят весь фрагмент в DOM
+movieApi
+  .fetchPopularMoviesList()
+  .then(createGallery)
+  .then(fragment => renderGallery(fragment, homePageRef));
