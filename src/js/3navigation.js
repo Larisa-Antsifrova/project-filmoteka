626e92098e4e00eba60c9e0f7dbdcf7cd43dda88
Kramskiy Dima <64987701+ifinddima@users.noreply.github.com>
2021-01-14 00:29:37 +0200

    adds functions for navigation-site

--- src/js/3navigation.js
+++ src/js/3navigation.js
@@ -7,13 +7,78 @@ const toggleQueueBtn = document.querySelector('[data-toggle-queue]');
 const toggleWatchedBtn = document.querySelector('[data-toggle-watched]');
 const watchedBtn = document.querySelector('[data-action-watched]');
 const queueBtn = document.querySelector('[data-action-queue]');
+const logoRefs = document.querySelector('.logo__js');
+const navigationRefs = document.querySelector('.navigation');
 
-// console.log(homeRef);
-// console.log(libraryRef);
-// console.log(librarySectionRef);
-// console.log(homePageSectionRef);
-// console.log(detailisSectionRef);
-// console.log(toggleQueueBtn);
-// console.log(toggleWatchedBtn);
-// console.log(watchedBtn);
-// console.log(queueBtn);
+// создаем глобальную переменную selectFilm
+let selectFilm = {};
+
+//вешаем слушатели
+homeRef.addEventListener('click', activeHomePage);
+logoRefs.addEventListener('click', activeHomePage);
+libraryRef.addEventListener('click', activeLibraryPage);
+
+//создаем функцию activeHomePage которая показывает домашнюю страницу и прячет остальные
+function activeHomePage(e) {
+  e.preventDefault();
+  toggleActiveLink(homeRef);
+
+  homePageSectionRef.classList.remove('is-hidden');
+  librarySectionRef.classList.add('is-hidden');
+  detailisSectionRef.classList.add('is-hidden');
+}
+//создаем функцию activeLibraryPage которая показывает домашнюю страницу и прячет остальные
+function activeLibraryPage(e) {
+  e.preventDefault();
+  toggleActiveLink(libraryRef);
+  librarySectionRef.classList.remove('is-hidden');
+  homePageSectionRef.classList.add('is-hidden');
+  detailisSectionRef.classList.add('is-hidden');
+  drawQueueFilmList();
+}
+
+// renderFilms.then(data => {
+//   return data.find(el => {
+//     if (el.id === 508442) {
+//       return el;
+//     }
+//   });
+// });
+
+// selectFilm = renderFilms.then(data => {
+//   return data.find(el => {
+//     if (el.id === 508442) {
+//       return el;
+//     }
+//   });
+// });
+
+// selectFilm.then(console.log);
+
+// создаем функцию activeDetailsPage которая показывает страницу детальной отрисовки фильма
+// function activeDetailsPage(movieId, itsLibraryFilm) {
+//   detailisSectionRef.classList.remove('is-hidden');
+//   homePageSectionRef.classList.add('is-hidden');
+//     librarySectionRef.classList.add('is-hidden');
+//     if (itsLibraryFilm) {
+//         selectFilm =
+//     }
+// }
+
+// activeDetailsPage();
+function toggleActiveLink(link) {
+  const currentActiveLink = navigationRefs.querySelector('.current');
+  if (currentActiveLink) {
+    currentActiveLink.classList.remove('current');
+  }
+  link.classList.add('current');
+}
+// renderFilms.then(console.log);
+
+// console.log(homePageRef);
+// homePageRef.addEventListener('click', e => {
+//   if (e.target.nodeName !== 'IMG') {
+//     return;
+//   }
+//   movieId = e.target.getAttribute('data-id');
+// });
